62fadfb02fc4809651cb9cc06065ca7c4f065323
wienart1312 <97341642+wienart1312@users.noreply.github.com>
2022-01-08 20:35:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    titleWeb : "Gửi cho bé của tui moahhh"
+    introTitle      '' patpat  đángiu
+    introDesc :        Trái   đất  vốn  lạ  thường
+    Mà sao  em  cứ  đi nhầm  lạ    đường
+    Lạc  vào  tim  anh  lẻ  loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
